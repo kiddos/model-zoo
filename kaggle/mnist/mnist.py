369019cc77d5de7c8c5249faee4af309f7badc6e
kiddos <kiddo831007@gmail.com>
2017-12-22 20:56:45 +0800

    rework mnist

--- kaggle/mnist/mnist.py
+++ kaggle/mnist/mnist.py
@@ -3,262 +3,209 @@ import numpy as np
 import logging
 import time
 import os
+from argparse import ArgumentParser
+
+from mnist_prepare import MNISTData
 
 
-class MNISTConvolutionModel(object):
-  def __init__(self, input_width, input_height, input_channel, output_size,
-      model_name='MNISTConvolutionModel', learning_rate=1e-3,
-      decay=0.9, saving=True):
-    # logger
-    logging.basicConfig()
-    self.logger = logging.getLogger(model_name)
-    self.logger.setLevel(logging.INFO)
-    self.logger.info('setting up model...')
-    # model
-    with tf.variable_scope(model_name):
-      self.inputs, self.labels, self.keep_prob, \
-        self.outputs, self.loss, self.accuracy, self.train_op = \
-        self._build_model(input_width, input_height, input_channel,
-        output_size, learning_rate, decay)
-    # checkpoint
-    self.start_epoch = 0
-    self.saving = saving
-    if saving:
-      self.checkpoint_path, summary_path = self._prepare_save_dir(model_name)
-      # saver
-      self.logger.info('setting up saver...')
-      self.saver = tf.train.Saver()
-      # summary writer
-      self.logger.info('setting up summary writer...')
-      self.summary_writer = tf.summary.FileWriter(summary_path,
-        tf.get_default_graph())
-    self.merged_summary = tf.summary.merge_all()
+logging.basicConfig()
+logger = logging.getLogger('mnist')
+logger.setLevel(logging.INFO)
 
-  def _prepare_save_dir(self, model_name):
-    index = 0
-    while os.path.isdir(model_name + str(index)):
-      index += 1
-    model_path = model_name + str(index)
-    self.logger.info('creating model directory %s...' % (model_path))
-    os.mkdir(model_path)
-    summary_path = os.path.join(model_path, 'summary')
-    os.mkdir(summary_path)
-    checkpoint_path = os.path.join(model_path, model_name)
-    return checkpoint_path, summary_path
 
-  def _build_model(self, input_width, input_height, input_channel,
-    output_size, learning_rate, decay):
-    # inputs
-    inputs = tf.placeholder(dtype=tf.float32,
-    shape=[None, input_height, input_width, input_channel])
-    labels = tf.placeholder(dtype=tf.float32, shape=[None, output_size])
-    keep_prob = tf.placeholder(dtype=tf.float32, shape=())
-    tf.summary.image(name='input_images', tensor=inputs)
-    tf.summary.histogram(name='labels', values=labels)
-    # learning rate
-    self.learning_rate = tf.Variable(learning_rate, name='learning_rate',
-      trainable=False)
-    self.decay_lr = tf.assign(self.learning_rate, self.learning_rate * decay)
-    tf.summary.scalar(name='learning_rate', tensor=self.learning_rate)
-    # model
-    with tf.name_scope('conv1'):
-      h1_size = 32
-      w = tf.get_variable(name='conv_w1',
-        shape=[7, 7, input_channel, h1_size],
-        dtype=tf.float32,
-        initializer=tf.random_normal_initializer(stddev=0.025))
-      b = tf.get_variable(name='conv_b1', shape=[h1_size],
-        dtype=tf.float32,
-        initializer=tf.constant_initializer(value=1e-3))
-      h = tf.nn.relu(tf.nn.conv2d(inputs, w, strides=[1, 1, 1, 1],
-        padding='SAME') + b)
-      h = tf.nn.max_pool(h, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1],
-        padding='SAME')
-      #  h = tf.nn.dropout(h, keep_prob)
-    with tf.name_scope('conv2'):
-      h2_size = 32
-      w = tf.get_variable(name='conv_w2',
-        shape=[5, 5, h1_size, h2_size],
-        dtype=tf.float32,
-        initializer=tf.random_normal_initializer(stddev=0.05))
-      b = tf.get_variable(name='conv_b2', shape=[h2_size],
-        dtype=tf.float32,
-        initializer=tf.constant_initializer(value=1e-3))
-      h = tf.nn.relu(tf.nn.conv2d(h, w, strides=[1, 1, 1, 1],
-        padding='SAME') + b)
-      h = tf.nn.max_pool(h, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1],
-        padding='SAME')
-      #  h = tf.nn.dropout(h, keep_prob)
-    with tf.name_scope('conv3'):
-      h3_size = 64
-      w = tf.get_variable(name='conv_w3',
-        shape=[5, 5, h2_size, h3_size],
-        dtype=tf.float32,
-        initializer=tf.random_normal_initializer(stddev=0.03))
-      b = tf.get_variable(name='conv_b3', shape=[h3_size],
-        dtype=tf.float32,
-        initializer=tf.constant_initializer(value=1e-3))
-      h = tf.nn.relu(tf.nn.conv2d(h, w, strides=[1, 1, 1, 1],
-        padding='SAME') + b)
-      h = tf.nn.max_pool(h, strides=[1, 2, 2, 1], ksize=[1, 2, 2, 1],
-        padding='SAME')
-      #  h = tf.nn.dropout(h, keep_prob)
-    with tf.name_scope('conv4'):
-      h4_size = 64
-      w = tf.get_variable(name='conv_w4',
-        shape=[5, 5, h3_size, h4_size],
-        dtype=tf.float32,
-        initializer=tf.random_normal_initializer(stddev=0.03))
-      b = tf.get_variable(name='conv_b4', shape=[h4_size],
-        dtype=tf.float32,
-        initializer=tf.constant_initializer(value=1e-3))
-      h = tf.nn.relu(tf.nn.conv2d(h, w, strides=[1, 1, 1, 1],
-        padding='SAME') + b)
-      #  h = tf.nn.dropout(h, keep_prob)
-    with tf.name_scope('conv5'):
-      h5_size = 64
-      w = tf.get_variable(name='conv_w5',
-        shape=[5, 5, h4_size, h5_size],
-        dtype=tf.float32,
-        initializer=tf.random_normal_initializer(stddev=0.03))
-      b = tf.get_variable(name='conv_b5', shape=[h5_size],
-        dtype=tf.float32,
-        initializer=tf.constant_initializer(value=1e-3))
-      h = tf.nn.relu(tf.nn.conv2d(h, w, strides=[1, 1, 1, 1],
-        padding='SAME') + b)
-      #  h = tf.nn.dropout(h, keep_prob)
-    with tf.name_scope('conv6'):
-      h6_size = 64
-      w = tf.get_variable(name='conv_w6',
-        shape=[5, 5, h5_size, h6_size],
-        dtype=tf.float32,
-        initializer=tf.random_normal_initializer(stddev=0.03))
-      b = tf.get_variable(name='conv_b6', shape=[h4_size],
-        dtype=tf.float32,
-        initializer=tf.constant_initializer(value=1e-3))
-      h = tf.nn.relu(tf.nn.conv2d(h, w, strides=[1, 1, 1, 1],
-        padding='SAME') + b)
-      #  h = tf.nn.dropout(h, keep_prob)
-    with tf.name_scope('fc7'):
-      h_shape = h.get_shape().as_list()
-      connect_size = h_shape[1] * h_shape[2] * h_shape[3]
-      h7_size = 1024
-      w = tf.get_variable(name='w7',
-        shape=[connect_size, h7_size],
-        dtype=tf.float32,
-        initializer=tf.random_normal_initializer(
-          stddev=np.sqrt(2.0 / connect_size)))
-      b = tf.get_variable(name='b7', shape=[h7_size],
-        dtype=tf.float32,
-        initializer=tf.constant_initializer(value=1e-3))
-      h = tf.nn.relu(tf.matmul(tf.reshape(h, [-1, connect_size]), w) + b)
-    with tf.name_scope('fc8'):
-      h8_size = 256
-      w = tf.get_variable(name='w8',
-        shape=[h7_size, h8_size],
-        dtype=tf.float32,
-        initializer=tf.random_normal_initializer(
-          stddev=np.sqrt(2.0 / connect_size)))
-      b = tf.get_variable(name='b8', shape=[h8_size],
-        dtype=tf.float32,
-        initializer=tf.constant_initializer(value=1e-3))
-      h = tf.nn.relu(tf.matmul(h, w) + b)
-    with tf.name_scope('output'):
-      w = tf.get_variable(name='ow', shape=[h8_size, output_size],
-        dtype=tf.float32,
-        initializer=tf.random_normal_initializer(
-          stddev=np.sqrt(2.0 / h5_size)))
-      b = tf.get_variable(name='ob', shape=[output_size],
-        dtype=tf.float32,
-        initializer=tf.constant_initializer(value=1e-3))
-      logits = tf.matmul(h, w) + b
-      outputs = tf.nn.softmax(logits)
-      tf.summary.histogram(name='outputs', values=outputs)
-    # loss and optimizer
+class MNIST(object):
+  def __init__(self, inference, learning_rate=1e-3, saving=True):
+
+    with tf.name_scope('inputs'):
+      self._setup_inputs()
+
+    tf.summary.image(name='input_images', tensor=self.input_images)
+    tf.summary.histogram(name='labels', values=self.labels)
+
+    self.inference_name = inference
+
+    if hasattr(self, inference):
+      inference_fn = getattr(self, inference)
+    with tf.variable_scope('mnist'):
+      logits, self.output = inference_fn(self.input_images)
+
     with tf.name_scope('loss'):
-      loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
-        logits=logits, labels=labels))
-      tf.summary.scalar(name='loss', tensor=loss)
-      accuracy = tf.reduce_sum(tf.cast(tf.equal(
-        tf.argmax(outputs, axis=1), tf.argmax(labels, axis=1)), tf.float32)) * \
-        100.0 / tf.cast(tf.shape(labels)[0], tf.float32)
-      tf.summary.scalar(name='accuracy', tensor=accuracy)
+      self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
+        logits=logits, labels=self.labels))
+    tf.summary.scalar('loss', self.loss)
+
     with tf.name_scope('optimizer'):
+      self.learning_rate = tf.Variable(learning_rate, trainable=False,
+        name='learning_rate')
       optimizer = tf.train.GradientDescentOptimizer(self.learning_rate)
-      train_op = optimizer.minimize(loss)
-    return inputs, labels, keep_prob, outputs, loss, accuracy, train_op
+      self.train_ops = optimizer.minimize(self.loss)
 
-  def load(self, sess, checkpoint_path):
-    if os.path.isfile(checkpoint_path + '.meta') and \
-        os.path.isfile(checkpoint_path + '.index'):
-      self.logger.info('loading last %s checkpoint...' % (checkpoint_path))
-      self.saver.restore(sess, checkpoint_path)
-      self.start_epoch = int(checkpoint_path.split('-')[-1].strip())
-    else:
-      self.logger.warning('%s does not exists' % (checkpoint_path))
+      self.decay_lr = tf.assign(self.learning_rate,
+        self.learning_rate * 0.9, name='decay_learning_rate')
 
-  def train(self, sess, data, label,
-      batch_size=1024, output_period=1000,
-      keep_prob=0.8, max_epoch=10000,
-      decay_epoch=1000):
-    # initialize
-    if self.start_epoch == 0:
-      self.logger.info('initializing variables...')
-      sess.run(tf.global_variables_initializer())
-    # training
-    self.logger.info('start training...')
-    last = time.time()
-    for epoch in range(self.start_epoch, self.start_epoch + max_epoch + 1):
-      # prepare batch data
-      offset = (epoch * batch_size) % (data.shape[0] - batch_size + 1)
-      batch_data = data[offset:offset+batch_size, :]
-      batch_label = label[offset:offset+batch_size, :]
-      _, loss = sess.run([self.train_op, self.loss], feed_dict={
-        self.inputs: batch_data,
-        self.labels: batch_label,
-        self.keep_prob: keep_prob
+    with tf.name_scope('summary'):
+      self.merged_summary = tf.summary.merge_all()
+
+  def prepare_folder(self):
+    index = 0
+    folder = 'mnist_%s_%d' % (self.inference_name, index)
+    while os.path.isdir(folder):
+      index += 1
+      folder = 'mnist_%s_%d' % (self.inference_name, index)
+    os.mkdir(folder)
+    return folder
+
+  def _setup_inputs(self):
+    self.input_images = tf.placeholder(dtype=tf.float32, name='input_images',
+      shape=[None, 28, 28, 1])
+    self.labels = tf.placeholder(dtype=tf.float32, name='labels',
+      shape=[None, 10])
+    self.keep_prob = tf.placeholder(dtype=tf.float32, shape=(), name='keep_prob')
+
+  def inference_v0(self, inputs):
+    with tf.name_scope('conv1'):
+      conv = tf.contrib.layers.conv2d(inputs, 32, stride=1, kernel_size=3,
+        weights_initializer=tf.random_normal_initializer(stddev=0.05))
+
+    with tf.name_scope('pool1'):
+      pool = tf.contrib.layers.max_pool2d(conv, 2)
+
+    with tf.name_scope('conv2'):
+      conv = self.multiple_conv(pool, 64, 3)
+
+    with tf.name_scope('pool2'):
+      pool = tf.contrib.layers.max_pool2d(conv, 2)
+
+    with tf.name_scope('conv3'):
+      conv = self.multiple_conv(pool, 128, 3)
+
+    with tf.name_scope('pool3'):
+      pool = tf.contrib.layers.max_pool2d(conv, 2)
+
+    with tf.name_scope('conv4'):
+      conv = self.multiple_conv(pool, 256, 3)
+
+    with tf.name_scope('pool4'):
+      pool = tf.contrib.layers.max_pool2d(conv, 2)
+
+    with tf.name_scope('drop4'):
+      drop = tf.nn.dropout(pool, keep_prob=self.keep_prob)
+
+    with tf.name_scope('fully_connected'):
+      connect_shape = drop.get_shape().as_list()
+      connect_size = connect_shape[1] * connect_shape[2] * connect_shape[3]
+      fc = tf.contrib.layers.fully_connected(
+        tf.reshape(drop, [-1, connect_size]), 1024,
+        weights_initializer=tf.variance_scaling_initializer())
+
+    with tf.name_scope('output'):
+      logits = tf.contrib.layers.fully_connected(fc, 10,
+        activation_fn=None,
+        weights_initializer=tf.variance_scaling_initializer())
+      outputs = tf.nn.softmax(logits)
+    return logits, outputs
+
+  def multiple_conv(self, inputs, size, ksize, multiple=1):
+    conv = tf.contrib.layers.conv2d(inputs, size, stride=1, kernel_size=ksize,
+      weights_initializer=tf.variance_scaling_initializer())
+    for i in range(multiple):
+      conv = tf.contrib.layers.conv2d(conv, size / 2, stride=1, kernel_size=1,
+        weights_initializer=tf.variance_scaling_initializer())
+      conv = tf.contrib.layers.conv2d(conv, size, stride=1, kernel_size=ksize,
+        weights_initializer=tf.variance_scaling_initializer())
+    return conv
+
+
+def train(args):
+  mnist_data = MNISTData(args.dbname)
+
+  training_data, training_label = mnist_data.get_training_data()
+  validation_data, validation_label = mnist_data.get_validation_data()
+
+  train_size = len(training_data)
+
+  model = MNIST(args.inference, learning_rate=args.learning_rate)
+
+  config = tf.ConfigProto()
+  config.gpu_options.allow_growth = True
+
+  saving = args.saving == 'True'
+  if saving:
+    folder = model.prepare_folder()
+    checkpoint = os.path.join(folder, 'mnist')
+    saver = tf.train.Saver()
+    summary_writer = tf.summary.FileWriter(os.path.join(folder, 'summary'),
+      tf.get_default_graph())
+
+  with tf.Session(config=config) as sess:
+    logger.info('initializing variables...')
+    sess.run(tf.global_variables_initializer())
+
+    logger.info('start training...')
+    for epoch in range(args.max_epoches):
+      # preprare data
+      offset = epoch % (train_size - args.batch_size)
+      data_batch = training_data[offset:offset+args.batch_size, :]
+      label_batch = training_label[offset:offset+args.batch_size, :]
+
+      if epoch % args.display_epoches == 0:
+        loss = sess.run(model.loss, feed_dict={
+          model.input_images: data_batch,
+          model.labels: label_batch,
+          model.keep_prob: args.keep_prob,
+        })
+        logger.info('%d. loss: %f', epoch, loss)
+
+      # train
+      sess.run(model.train_ops, feed_dict={
+        model.input_images: data_batch,
+        model.labels: label_batch,
+        model.keep_prob: args.keep_prob,
       })
-      # output
-      if epoch % output_period == 0:
-        feed_dict={
-          self.inputs: batch_data,
-          self.labels: batch_label,
-          self.keep_prob: 1.0
-        }
-        ms, loss, accuracy = sess.run(
-          [self.merged_summary, self.loss, self.accuracy], feed_dict)
-        self.logger.info('%d. loss: %f | accuracy: %f | time used: %f' %
-          (epoch, loss, accuracy, (time.time() - last)))
-        last = time.time()
-        if self.saving:
-          self.saver.save(sess, self.checkpoint_path, global_step=epoch)
-          self.summary_writer.add_summary(ms, global_step=epoch)
-      if epoch % decay_epoch == 0 and epoch != 0:
-        sess.run(self.decay_lr)
 
-  def predict(self, sess, data):
-    return sess.run(self.outputs, feed_dict={
-      self.inputs: data,
-      self.keep_prob: 1.0
-    })
+      if epoch % args.save_epoches == 0 and epoch != 0 and saving:
+        logger.info('saving model...')
+        saver.save(sess, checkpoint, global_step=epoch)
+
+      if epoch % args.summary_epoches == 0 and epoch != 0 and saving:
+        summary = sess.run(model.summary, feed_dict={
+          model.input_images: data_batch,
+          model.labels: label_batch,
+          model.keep_prob: 1.0,
+        })
+        summary_writer.add_summary(summary, global_step=epoch)
 
 
-def test():
-  image_size = 28
-  image_channel = 1
-  data = np.random.randn(256, image_size, image_size, image_channel)
-  label = np.zeros(shape=[256, 10])
-  label[:, 0] = 1.0
-  model = MNISTConvolutionModel(image_size, image_size, image_channel,
-    10, model_name='test', saving=False)
+def main():
+  parser = ArgumentParser()
+  parser.add_argument('--dbname', dest='dbname', default='mnist.sqlite3',
+    type=str, help='sqlite3 db to load for training')
 
-  with tf.Session() as sess:
-    model.train(sess, data, label,
-      batch_size=32,
-      output_period=10,
-      keep_prob=0.8,
-      max_epoch=100)
+  parser.add_argument('--inference', dest='inference', default='inference_v0',
+    type=str, help='inference model version')
+
+  parser.add_argument('--learning-rate', dest='learning_rate', default=1e-3,
+    type=float, help='learning rate to train model')
+  parser.add_argument('--keep-prob', dest='keep_prob', default=0.8,
+    type=float, help='keep probability for dropouts')
+  parser.add_argument('--max-epoches', dest='max_epoches', default=100000,
+    type=int, help='max epoches to train model')
+  parser.add_argument('--save-epoches', dest='save_epoches', default=10000,
+    type=int, help='epoches to save model')
+  parser.add_argument('--display-epoches', dest='display_epoches', default=100,
+    type=int, help='epoches to display')
+  parser.add_argument('--decay-epoches', dest='decay_epoches', default=5000,
+    type=int, help='epoches to decay learning rate')
+  parser.add_argument('--summary-epoches', dest='summary_epoches', default=100,
+    type=int, help='epoches to save summary for model')
+  parser.add_argument('--batch-size', dest='batch_size', default=32,
+    type=int, help='batch size for training')
+  parser.add_argument('--saving', dest='saving', default='False',
+    type=str, help='save model')
+  args = parser.parse_args()
+
+  train(args)
 
 
 if __name__ == '__main__':
-  test()
+  main()
--- kaggle/mnist/mnist_prepare.py
+++ kaggle/mnist/mnist_prepare.py
@@ -1,110 +1,157 @@
-# -*- coding: utf-8 -*-
-
-import sqlite3
-import os
+from urllib2 import urlopen
 import logging
+import os
+import gzip
+import struct
 import numpy as np
+import sqlite3
+import unittest
 from argparse import ArgumentParser
 
 
+URL = 'http://yann.lecun.com/exdb/mnist/'
+FILES = [
+  'train-images-idx3-ubyte.gz',
+  'train-labels-idx1-ubyte.gz',
+  't10k-images-idx3-ubyte.gz',
+  't10k-labels-idx1-ubyte.gz']
+
+
 logging.basicConfig()
-logger = logging.getLogger('mnist_prepare')
+logger = logging.getLogger('mnist')
 logger.setLevel(logging.INFO)
 
 
-def read_train_data(path):
-  labels = []
-  images = []
-  if path and os.path.isfile(path):
-    logger.info('reading %s' % (path))
-    with open(path, 'r') as f:
-      f.readline()
-      while True:
-        line = f.readline()
-        if not line: break
-        entry = line.split(',')
-        label = int(entry[0])
-        image = np.array([int(b) for b in entry[1:]], dtype=np.uint8)
-        labels.append(label)
-        images.append(image)
-  else:
-    logger.warning('%s not found' % (path))
-  return images, labels
+def download_files(folder):
+  if not os.path.isdir(folder):
+    os.mkdir(folder)
+
+  for filename in FILES:
+    filepath = os.path.join(folder, filename)
+    if not os.path.isfile(filepath):
+      logger.info('downloading %s...', filename)
+      url = URL + filename
+      content = urlopen(url).read()
+      with open(filepath, 'wb') as f:
+        f.write(content)
 
 
-def read_test_data(path):
-  images = []
-  if path and os.path.isfile(path):
-    logger.info('reading %s' % (path))
-    with open(path, 'r') as f:
-      f.readline()
-      while True:
-        line = f.readline()
-        if not line: break
-        entry = line.split(',')
-        image = np.array([int(b) for b in entry], dtype=np.uint8)
-        images.append(image)
-  else:
-    logger.warning('%s not found' % (path))
-  return images
-
-
-def save(train_data, train_label, test_data, name='mnist.sqlite3'):
-  if not os.path.isfile(name):
-    logger.info('saving into %s...' % (name))
-    connection = sqlite3.connect(name)
-    cursor = connection.cursor()
-    # train data
-    cursor.execute("""CREATE TABLE IF NOT EXISTS train(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        image BLOB NOT NULL,
-        label INTEGER NOT NULL);""")
-    for i in range(len(train_data)):
-      td = buffer(train_data[i])
-      tl = train_label[i]
-      cursor.execute("""INSERT INTO train(image, label) VALUES(?, ?);""",
-          (td, tl,))
-    # test data
-    cursor.execute("""CREATE TABLE IF NOT EXISTS test(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        image BLOB NOT NULL);""")
-    for i in range(len(test_data)):
-      td = buffer(test_data[i])
-      cursor.execute("""INSERT INTO test(image) VALUES(?);""",
-          (td,))
-    connection.commit()
-    connection.close()
-
-
-def load(db):
-  connection = sqlite3.connect(db)
+def parse_image(dbname, tablename, image_path, label_path):
+  logger.info('inserting data for %s...', tablename)
+  connection = sqlite3.connect(dbname)
   cursor = connection.cursor()
-  cursor.execute("""SELECT image, label FROM train;""")
-  train_data = cursor.fetchall()
-  cursor.execute("""SELECT image FROM test;""")
-  test_data = cursor.fetchall()
+  cursor.execute("""DROP TABLE IF EXISTS %s;""" % (tablename))
+  cursor.execute("""CREATE TABLE %s(
+    image BLOB NOT NULL,
+    label INTEGER NOT NULL);""" % (tablename))
+
+  with gzip.open(image_path, 'rb') as image_file:
+    image_content = image_file.read()
+
+    header = struct.unpack('>iiii', image_content[:16])
+    assert header[0] == 2051
+
+    count = header[1]
+    height, width = header[2:]
+    assert height == width
+    assert width == 28
+    size = height * width
+
+    with gzip.open(label_path, 'rb') as label_file:
+      label_content = label_file.read()
+
+      header = struct.unpack('>ii', label_content[:8])
+      assert header[0] == 2049
+      assert header[1] == count
+
+      for i in range(count):
+        offset = 16 + size * i
+        image_data = image_content[offset:offset+size]
+
+        offset = 8 + i
+        label_data = ord(label_content[offset:offset+1])
+        cursor.execute("""INSERT INTO %s VALUES(?, ?)""" % (tablename),
+          (buffer(image_data), label_data))
+  connection.commit()
   connection.close()
 
-  train_images = []
-  train_labels = []
-  test_images = []
-  for entry in train_data:
-    train_images.append(np.array(entry[0], dtype=np.uint8).reshape(28, 28, 1))
-    train_labels.append([1 if l == entry[1] else 0 for l in range(10)])
-  for entry in test_data:
-    test_images.append(np.array(entry[0], dtype=np.uint8).reshape(28, 28, 1))
-  return np.array(train_images), np.array(train_labels), np.array(test_images)
+
+class MNISTData(object):
+  def __init__(self, dbname):
+    if os.path.isfile(dbname):
+      self.connection = sqlite3.connect(dbname)
+      self.cursor = self.connection.cursor()
+    else:
+      logger.info('fail to load %s.', dbname)
+
+    self._load_data()
+
+  def _load_data(self):
+    self.data = []
+    self.label = []
+
+    if hasattr(self, 'cursor'):
+      self.data, self.label = self._load_from_table('train')
+      self.test_data, self.test_label = self._load_from_table('test')
+
+      index = np.random.permutation(np.arange(len(self.data)))
+      self.data = self.data[index, :]
+      self.label = self.label[index, :]
+
+      self.training_data = self.data[:int(len(self.data) * 0.8)]
+      self.training_label = self.label[:int(len(self.data) * 0.8)]
+
+      self.validation_data = self.data[int(len(self.data) * 0.8):]
+      self.validation_label = self.label[int(len(self.data) * 0.8):]
+
+  def _load_from_table(self, table):
+    data = []
+    label = []
+    self.cursor.execute("""SELECT * FROM %s;""" % (table))
+    raw_data = self.cursor.fetchall()
+    for entry in raw_data:
+      img = np.frombuffer(entry[0], dtype=np.uint8).reshape([28, 28, 1])
+      data.append(img)
+
+      l = [0 for i in range(10)]
+      l[entry[1]] = 1.0
+      label.append(l)
+    return np.array(data, dtype=np.uint8), np.array(label, dtype=np.uint8)
+
+  def get_training_data(self):
+    return self.training_data, self.training_label
+
+  def get_validation_data(self):
+    return self.validation_data, self.validation_label
+
+class TestMNISTData(unittest.TestCase):
+  def setUp(self):
+    self.mnist_data = MNISTData('mnist.sqlite3')
+
+  def test_data_size(self):
+    self.assertTrue(len(self.mnist_data.data) == 60000)
+    self.assertTrue(len(self.mnist_data.label) == 60000)
+    self.assertTrue(len(self.mnist_data.test_data) == 10000)
+    self.assertTrue(len(self.mnist_data.test_label) == 10000)
 
 
 def main():
   parser = ArgumentParser()
-  parser.add_argument('--train', dest='train', help='train data csv')
-  parser.add_argument('--test', dest='test', help='test data csv')
-
+  parser.add_argument('--folder', dest='folder', default='data',
+    type=str, help='folder to download mnist data')
+  parser.add_argument('--dbname', dest='dbname', default='mnist.sqlite3',
+    type=str, help='output sqlite3 db')
   args = parser.parse_args()
-  data, label = read_train_data(args.train)
-  test_data = read_test_data(args.test)
-  save(data, label, test_data)
+
+  download_files(args.folder)
+
+  if not os.path.isfile(args.dbname):
+    parse_image(args.dbname, 'train',
+      os.path.join(args.folder, FILES[0]), os.path.join(args.folder, FILES[1]))
+    parse_image(args.dbname, 'test',
+      os.path.join(args.folder, FILES[2]), os.path.join(args.folder, FILES[3]))
+
+  unittest.main()
 
 
 if __name__ == '__main__':
