52064eaf3865a84cfc459442b34e4e1047302363
kiddos <kiddo831007@gmail.com>
2017-12-23 09:33:38 +0800

    remove download script

--- kaggle/mnist/download.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-if [ ! -f "train.csv" ]; then
-  wget --user="icalpublic" --ask-password "ftp://140.127.205.173/sda1/public/data/mnist/train.csv"
-fi
-
-if [ ! -f "test.csv" ]; then
-  wget --user="icalpublic" --ask-password "ftp://140.127.205.173/sda1/public/data/mnist/test.csv"
-fi
