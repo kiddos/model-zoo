330428a92742a1e261409315ad95d6f02177b162
kiddos <kiddo831007@gmail.com>
2018-02-08 12:13:19 +0800

    run the game

---
+++ breakout/run.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+import numpy as np
+import os
+import logging
+import gym
+import time
+
+from environment import SkipFrameEnvironment
+
+
+logging.basicConfig()
+logger = logging.getLogger('run')
+logger.setLevel(logging.INFO)
+
+
+FLAGS = tf.app.flags.FLAGS
+tf.app.flags.DEFINE_string('model', 'breakout.pb', 'model to run')
+
+
+def load_graph():
+  if os.path.isfile(FLAGS.model):
+    with tf.gfile.GFile(FLAGS.model, 'rb') as gf:
+      graph_def = tf.GraphDef()
+      graph_def.ParseFromString(gf.read())
+
+      with tf.Graph().as_default() as graph:
+        tf.import_graph_def(graph_def)
+        return graph
+  else:
+    logger.error('model %s not found', FLAGS.model)
+
+
+def main(_):
+  env = SkipFrameEnvironment('BreakoutNoFrameskip-v4', 4, 84, 84)
+
+  graph = load_graph()
+  input_state = graph.get_tensor_by_name('import/state:0')
+  q_values = graph.get_tensor_by_name('import/train/output/q_values:0')
+
+  config = tf.ConfigProto()
+  config.gpu_options.allow_growth = True
+  with tf.Session(config=config, graph=graph) as sess:
+    for i in range(100):
+      state = env.reset()
+      steps = 0
+      total_reward = 0
+      while True:
+        action_prob = sess.run(q_values, feed_dict={
+          input_state: np.expand_dims(state, axis=0)
+        })
+        action = np.argmax(action_prob[0, :])
+        state, reward, done = env.step(action)
+        assert state.shape[2] == 4
+        steps += 1
+        total_reward += reward
+
+        env.render()
+        time.sleep(0.066)
+
+        if done:
+          break
+
+
+if __name__ == '__main__':
+  tf.app.run()
