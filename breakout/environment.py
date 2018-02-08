5758dde81c138f7f3ba4f4a8179c1c512b329929
kiddos <kiddo831007@gmail.com>
2018-02-08 19:38:51 +0800

    add history frame environment

--- breakout/environment.py
+++ breakout/environment.py
@@ -2,6 +2,18 @@ import gym
 import numpy as np
 from random import randint
 from PIL import Image
+from collections import deque
+import time
+import cv2
+
+
+def process_image(state, input_width, input_height):
+  image = Image.fromarray(state).crop([8, 32, 152, 210])
+  #  image = image.resize([68, 65]).convert('L')
+  image = image.resize([input_width, input_height],
+    Image.NEAREST).convert('L')
+  img = np.expand_dims(np.array(image, dtype=np.uint8), axis=2)
+  return img
 
 
 class SkipFrameEnvironment(object):
@@ -12,52 +24,90 @@ class SkipFrameEnvironment(object):
     self.env = gym.make(name)
     self.action_size = self.env.action_space.n
     self.states = []
+    self.lives = 0
 
   def reset(self):
-    state = self.env.reset()
-    R = 0
-    end = False
-    states = [self.process_image(state)]
-    for i in range(self.skip - 1):
-      state, reward, done, _ = self.env.step(randint(0, self.action_size - 1))
-      R += reward
-      states.append(self.process_image(state))
-      end |= done
+    states = deque(maxlen=self.skip)
+    if self.lives == 0:
+      state = self.env.reset()
+      states.append(process_image(state, self.image_width, self.image_height))
 
-    if end:
-      return self.reset()
-    else:
-      return np.concatenate(states, axis=2)
+    end = False
+    noop = 0
+    while len(states) < self.skip:
+      state, _, done, info = self.env.step(noop)
+      states.append(process_image(state, self.image_width, self.image_height))
+      end |= done
+      self.lives = info['ale.lives']
+    return np.concatenate(states, axis=2)
 
   def step(self, action):
-    states = []
+    states = deque(maxlen=self.skip)
     R = 0
     end = False
     for i in range(self.skip):
       state, reward, done, info = self.env.step(action)
-      states.append(self.process_image(state))
+      states.append(process_image(state, self.image_width, self.image_height))
       R += reward
       end |= done
-    return np.concatenate(states, axis=2), R, end, info['ale.lives']
 
-  def process_image(self, state):
-    image = Image.fromarray(state).crop([8, 32, 152, 210])
-    #  image = image.resize([68, 65]).convert('L')
-    image = image.resize([self.image_width, self.image_height],
-      Image.NEAREST).convert('L')
-    img = np.expand_dims(np.array(image, dtype=np.uint8), axis=2)
-    return img
+      if info['ale.lives'] < self.lives:
+        end |= True
+      self.lives = info['ale.lives']
+    return np.concatenate(states, axis=2), R, end, info['ale.lives']
 
   def render(self):
     self.env.render()
 
 
+class HistoryFrameEnvironment(object):
+  def __init__(self, name, history_size, image_width, image_height):
+    self.history = deque(maxlen=history_size)
+    self.image_width = image_width
+    self.image_height = image_height
+    self.env = gym.make(name)
+    self.action_size = self.env.action_space.n
+    self.states = []
+    self.lives = 0
+
+    for _ in range(history_size):
+      self.history.append(np.zeros([image_width, image_height, 1],
+        dtype=np.uint8))
+
+  def reset(self):
+    if self.lives == 0:
+      state = self.env.reset()
+      self.history.append(process_image(
+        state, self.image_width, self.image_height))
+    else:
+      noop = 0
+      state, _, _, info = self.env.step(noop)
+      self.lives = info['ale.lives']
+    return np.concatenate(self.history, axis=2)
+
+  def step(self, action):
+    state, reward, done, info = self.env.step(action)
+    self.history.append(process_image(
+      state, self.image_width, self.image_height))
+    done = info['ale.lives'] < self.lives
+    self.lives = info['ale.lives']
+    return np.concatenate(self.history, axis=2), \
+      reward, done, info['ale.lives']
+
+  def render(self):
+    self.env.render()
+
+
+
 def main():
-  env = SkipFrameEnvironment('BreakoutNoFrameskip-v4', 4, 84, 84)
+  #  env = SkipFrameEnvironment('BreakoutNoFrameskip-v4', 4, 84, 84)
+  env = HistoryFrameEnvironment('BreakoutDeterministic-v4', 4, 84, 84)
 
   for i in range(100):
     state = env.reset()
+    assert state.shape[2] == 4
     steps = 0
+
     total_reward = 0
     while True:
       action = randint(0, 3)
