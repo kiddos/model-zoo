17d2dcbaad5b7027453719ba6cf9b4a08c0a1170
kiddos <kiddo831007@gmail.com>
2018-02-10 00:20:06 +0800

    deeper model

--- breakout/dqn.py
+++ breakout/dqn.py
@@ -87,24 +87,31 @@ class DQN(object):
   def inference(self, inputs, trainable=True):
     with tf.name_scope('norm'):
       inputs = tf.div(inputs, 255.0)
+      images = tf.split(inputs, [1, 1, 1, 1], axis=3)
+      tf.summary.image('input_images', images[0])
 
     with tf.name_scope('conv1'):
-      conv = tf.contrib.layers.conv2d(inputs, 16, stride=4, kernel_size=8,
+      conv = tf.contrib.layers.conv2d(inputs, 32, stride=4, kernel_size=8,
         trainable=trainable, padding='VALID',
         weights_initializer=tf.variance_scaling_initializer())
 
     with tf.name_scope('conv2'):
-      conv = tf.contrib.layers.conv2d(conv, 32, stride=2, kernel_size=4,
+      conv = tf.contrib.layers.conv2d(conv, 64, stride=2, kernel_size=4,
+        trainable=trainable, padding='VALID',
+        weights_initializer=tf.variance_scaling_initializer())
+
+    with tf.name_scope('conv3'):
+      conv = tf.contrib.layers.conv2d(conv, 64, stride=1, kernel_size=3,
         trainable=trainable, padding='VALID',
         weights_initializer=tf.variance_scaling_initializer())
 
     with tf.name_scope('fully_connected'):
       fc = tf.contrib.layers.fully_connected(
-        tf.contrib.layers.flatten(conv), 256, trainable=trainable,
+        tf.contrib.layers.flatten(conv), 512, trainable=trainable,
         weights_initializer=tf.variance_scaling_initializer())
 
     with tf.name_scope('output'):
-      w = tf.get_variable('ow', shape=[256, self.config.action_size],
+      w = tf.get_variable('ow', shape=[512, self.config.action_size],
         initializer=tf.variance_scaling_initializer())
       b = tf.get_variable('ob', shape=[self.config.action_size],
         initializer=tf.zeros_initializer())
