4fb408eccd227e3fa3a4d8e5a42456aa37fc169a
kiddos <kiddo831007@gmail.com>
2018-02-28 05:25:29 +0800

    bug fix

--- breakout/replay_buffer.py
+++ breakout/replay_buffer.py
@@ -5,6 +5,8 @@ import gym
 from collections import deque
 from PIL import Image
 
+from environment import SimpleEnvironment
+
 
 class ReplayBuffer(object):
   def __init__(self, replay_buffer_size, image_width, image_height,
@@ -32,7 +34,7 @@ class ReplayBuffer(object):
     self.history.append(next_state)
 
     self._state[self._current_index, ...] = next_state
-    self._action[self._current_index] = np.array(action, np.int16)
+    self._action[self._current_index] = action
     self._reward[self._current_index] = np.sign(reward).astype(np.int16)
     self._done[self._current_index] = np.array(done, np.bool)
 
@@ -65,23 +67,24 @@ class ReplayBuffer(object):
     min_index = self.history_size
     if self._current_index < self.history_size:
       min_index *= 2
+    max_index = self._current_size - 1
     for b in range(batch_size):
-      index = random.randint(min_index, self._current_size - 1)
-      state = self.get_state(index - 1)
+      index = random.randint(min_index, max_index)
+      while self._done[index - 1]:
+        index = random.randint(min_index, max_index)
+
+      state = np.copy(self.get_state(index - 1))
       action = self._action[index]
-      next_state = self.get_state(index)
+      next_state = np.copy(self.get_state(index))
       over = self._done[index]
       reward = self._reward[index]
 
       # padd zero
       game_over = self._done[(index - self.history_size):index]
-      for i in range(-1, -self.history_size - 1, -1):
+      for i in range(self.history_size - 1, -1, -1):
         if game_over[i]:
-          if i == -1:
-            state[:, ...] = 0
-          else:
-            state[:(i + 1), ...] = 0
-          next_state[:i, ...] = 0
+          state[:, :, :(i + 1)] = 0
+          next_state[:, :, :i] = 0
           break
 
       states.append(state)
@@ -104,12 +107,12 @@ class ReplayBuffer(object):
 
 class TestReplayBuffer(unittest.TestCase):
   def setUp(self):
-    self.replay_buffer = ReplayBuffer(10, 84, 84, 4)
-    self.env = gym.make('Breakout-v0')
+    self.replay_buffer = ReplayBuffer(300, 84, 84, 4)
+    self.env = SimpleEnvironment('Breakout-v0')
 
   def test_add_states(self):
     state = self.env.reset()
-    self.replay_buffer.add(state, 0, 0, False)
+    self.replay_buffer.add_init_state(state)
     while True:
       action = random.randint(0, 3)
       next_state, reward, done, info = self.env.step(action)
@@ -118,8 +121,16 @@ class TestReplayBuffer(unittest.TestCase):
       if done: break
 
   def test_sample(self):
-    self.test_add_states()
-    states, actions, next_states, rewards, done = self.replay_buffer.sample(32)
+    try:
+      import cv2
+    except:
+      raise Exception
+
+    for _ in range(10):
+      self.test_add_states()
+
+    states, actions, next_states, rewards, done = \
+      self.replay_buffer.sample(300)
 
     self.assertEqual(states.dtype, np.uint8)
     self.assertEqual(next_states.dtype, np.uint8)
@@ -130,12 +141,40 @@ class TestReplayBuffer(unittest.TestCase):
     eq = np.all(states[:, :, :, 1:] == next_states[:, :, :, :3])
     self.assertTrue(eq)
 
-    #  p1 = Image.fromarray(states[0, :, :, 0])
-    #  p2 = Image.fromarray(next_states[0, :, :, 0])
-    #  p1.save('p1.png')
-    #  p2.save('p2.png')
+    self.assertEqual(len(states), len(next_states))
+
+    for i in range(len(states)):
+      display = np.zeros([84 * 2, 84 * 4, 1], np.uint8)
+
+      for j in range(4):
+        display[0:84, (j * 84):((j + 1) * 84), 0] = states[i, :, :, j]
+        display[84:, (j * 84):((j + 1) * 84), 0] = next_states[i, :, :, j]
+        cv2.line(display, (84 * j, 0), (84 * j, 168), (255, 255, 255), 1)
+
+      ACTION_MEANING = ['NOOP', 'FIRE', 'RIGHT', 'LEFT']
+      action_text = 'action: %s(%d)' % (ACTION_MEANING[actions[i]], actions[i])
+      cv2.putText(display, action_text, (0, 10),
+        cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), 1)
+      done_text = 'done: ' + str(done[i])
+      cv2.putText(display, done_text, (0, 30),
+        cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), 1)
+
+      cv2.imshow('States', display)
+      key = cv2.waitKey(0)
+      if key in [ord('q'), ord('Q')]: break
+
+
+      #  p1 = Image.fromarray(states[0, :, :, 0])
+      #  p2 = Image.fromarray(next_states[0, :, :, 0])
+      #  p1.save('p1.png')
+      #  p2.save('p2.png')
 
   def test_last_state(self):
+    try:
+      import cv2
+    except:
+      raise Exception
+
     state = self.env.reset()
     self.replay_buffer.add_init_state(state)
     while True:
@@ -151,16 +190,28 @@ class TestReplayBuffer(unittest.TestCase):
       self.assertTrue(eq)
 
       self.replay_buffer.add(next_state, action, reward, done)
+
+      display = np.zeros([84, 84 * 4, 1], np.uint8)
+      last_state = self.replay_buffer.last_state()
+
+      for j in range(4):
+        display[:, (j * 84):((j + 1) * 84), 0] = last_state[:, :, j]
+        cv2.line(display, (84 * j, 0), (84 * j, 84), (255, 255, 255), 1)
+
+      ACTION_MEANING = ['NOOP', 'FIRE', 'RIGHT', 'LEFT']
+      action_text = 'action: %s(%d)' % (ACTION_MEANING[action], action)
+      cv2.putText(display, action_text, (200, 10),
+        cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), 1)
+      done_text = 'done: ' + str(done)
+      cv2.putText(display, done_text, (230, 30),
+        cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 255, 255), 1)
+
+      cv2.imshow('States', display)
+      key = cv2.waitKey(1000)
+      if key in [ord('q'), ord('Q')]: break
+
       state = next_state
       if done: break
-
-  def test_multiple_sample(self):
-    for _ in range(20):
-      self.test_sample()
-
-  def test_multiple_last_state(self):
-    for _ in range(20):
-      self.test_last_state()
 
 
 def main():
