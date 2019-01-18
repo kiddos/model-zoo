035ccde7dff8b98ab34ae8d4c0a379d4783df6b6
kiddos <kiddo831007@gmail.com>
2019-01-18 13:10:24 +0800

    change parameters

--- rl/dqn.py
+++ rl/dqn.py
@@ -2,13 +2,12 @@ import tensorflow as tf
 
 
 FLAGS = tf.app.flags.FLAGS
-tf.app.flags.DEFINE_integer('num_actions', 4, 'number of actions')
 tf.app.flags.DEFINE_float('learning_rate', 1e-3, 'learning rate to train')
 tf.app.flags.DEFINE_float('gamma', 0.9, 'gamma')
 
 
 class DQN(object):
-  def __init__(self, input_shape, inference_fn):
+  def __init__(self, input_shape, inference_fn, num_actions):
     self.input_shape = input_shape
 
     with tf.name_scope('inputs'):
@@ -16,9 +15,10 @@ class DQN(object):
 
     self.inference = inference_fn
     q = self.inference(self.state, 'dqn')
-    o = tf.one_hot(self.action, FLAGS.num_actions)
+    o = tf.one_hot(self.action, num_actions)
     mask_q = tf.reduce_sum(o * q, axis=1)
-    self.best_action = tf.argmax(q, axis=1)
+    self.q_values = q
+    self.best_actions = tf.argmax(q, axis=1)
 
     tf.summary.scalar('q', tf.reduce_mean(mask_q))
 
@@ -36,6 +36,8 @@ class DQN(object):
       for i in range(len(v1)):
         self.copy.append(tf.assign(v2[i], v1[i]))
 
+    #  with tf.name_scope('loss'):
+    #    self.loss = tf.reduce_mean(tf.square(target - mask_q))
     self.loss = tf.losses.huber_loss(target, mask_q, scope='loss',
       reduction=tf.losses.Reduction.MEAN)
     tf.summary.scalar('loss', self.loss)
@@ -90,7 +92,7 @@ def atari_model(inputs, name):
 
 
 def test():
-  DQN([84, 84, 4], atari_model)
+  DQN([84, 84, 4], atari_model, 4)
 
 
 if __name__ == '__main__':
