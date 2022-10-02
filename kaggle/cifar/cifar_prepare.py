622546d0066543a3c0fcebc83fdb56c0456e13a5
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-02 19:38:08 +0000

    Adding tarfile member sanitization to extractall()

--- kaggle/cifar/cifar_prepare.py
+++ kaggle/cifar/cifar_prepare.py
@@ -43,7 +43,26 @@ def extract(tar_file, folder, output_folder):
   if not os.path.isdir(os.path.join(folder, output_folder)):
     logger.info('extracting %s...', output_folder)
     with tarfile.open(os.path.join(folder, tar_file), 'r:gz') as f:
-      f.extractall(folder)
+      def is_within_directory(directory, target):
+          
+          abs_directory = os.path.abspath(directory)
+          abs_target = os.path.abspath(target)
+      
+          prefix = os.path.commonprefix([abs_directory, abs_target])
+          
+          return prefix == abs_directory
+      
+      def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+      
+          for member in tar.getmembers():
+              member_path = os.path.join(path, member.name)
+              if not is_within_directory(path, member_path):
+                  raise Exception("Attempted Path Traversal in Tar File")
+      
+          tar.extractall(path, members, numeric_owner) 
+          
+      
+      safe_extract(f, folder)
   return os.path.join(folder, output_folder, 'train'), \
     os.path.join(folder, output_folder, 'test'), \
     os.path.join(folder, output_folder, 'meta')
